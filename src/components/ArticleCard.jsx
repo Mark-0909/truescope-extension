92d7fcfdf18bfefe92c4243b43f25bce30728e45
Mark Joshua Orcullo <orcullomark856@gmail.com>
2026-01-21 11:17:21 +0800

    added neutral for article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -21,12 +21,19 @@ export default function ArticleCard({oddEven, support, source, title, Remarks, V
                         </svg>
                         <p className="italic font-bold text-green-600 text-[10px] -mt-1">Support</p>
                     </>
-                ) : (
+                ) : support === 'refute' ? (
                     <>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-9 text-red-600">
                             <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
                         </svg>
                         <p className="italic font-bold text-red-600 text-[10px] -mt-1">Refute</p>
+                    </>
+                ) : (
+                    <>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-9 text-yellow-600">
+                            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3.75 9.75a.75.75 0 0 0 0-1.5H8.25a.75.75 0 0 0 0 1.5h7.5Z" clipRule="evenodd" />
+                        </svg>
+                        <p className="italic font-bold text-yellow-600 text-[10px] -mt-1">Neutral</p>
                     </>
                 )}
 
